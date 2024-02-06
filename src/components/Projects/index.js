9c108736553cfbc7245efbba64549c564b0f608e
mikesabzz <michaelsabzevari@gmail.com>
2024-02-06 16:40:15 -0500

    aligned cards

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -6,9 +6,9 @@ function Projects(props) {
   return (
     <div className="card h-100">
       <a href={URL} target="_blank" rel="noopener noreferrer">
-        <img src={imgURL} className="card-img-top img-fluid" alt={Title} />
+        <img src={imgURL} className="card-img-top img-fluid" alt={Title} style={{ height: "200px", objectFit: "cover" }} />
       </a>
-      <div className="card-body">
+      <div className="card-body d-flex flex-column justify-content-between">
         <a href={URL} target="_blank" rel="noopener noreferrer"className="card-title"><h5>{Title}</h5></a>
         <p className="card-text">{Desc}</p>
         <a href={Code} className="btn btn-primary" target="_blank" rel="noopener noreferrer">
